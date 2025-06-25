5644b7d64a7f775ecc901193bb9d61de5bbca999
rts-Liva <U2451012@rts-365.dk>
2025-06-25 09:15:58 +0200

    basis

---
+++ src/app/layout.js
@@ -0,0 +1,24 @@
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Quizz App",
+  description: "Our little quiz",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
